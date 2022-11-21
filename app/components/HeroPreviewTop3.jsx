7a1c917ee19ba2a0edfd52e29cef0cb252e3234e
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-11-21 23:05:56 +0200

    adding api for currencies

--- app/components/HeroPreviewTop3.jsx
+++ app/components/HeroPreviewTop3.jsx
@@ -1,64 +1,65 @@
 import Image from 'next/image';
-import EGPSVG from './EGPSVG';
 
+const baseUrl = 'https://api.exchangerate.host/latest';
 const currencies = [
     {
         name: 'EGP',
         icon: '/EGP.png',
-        url: 'https://api.exchangerate.host/latest?base=USD&symbols=EGP',
         value: 0,
-        code:'EGP'
+        base: 'USD',
+        symbols: 'EGP'
     },
     {
         name: 'Gold',
         icon: '/Gold.png',
-        url: 'https://api.metalpr{iceapi.com/v1/latest?api_key=0f898c804fc9668965af4a8d8247f8d7&base=XAU&currencies=EGP',
         value: 0,
-        code:'EGP'
+        base: 'BTC',
+        symbols: 'CAD'
+
 
     },
     {
         name: 'BTC',
         icon: '/BTC.png',
-        url: 'https://api.exchangerate.host/latest?base=BTC&symbols=EGP',
         value: 0,
-        code:'EGP'
+        base: 'BTC',
+        symbols: 'USD'
     }
 ]
 
-async function getData(url) {
+const getData = async (url) => {
     const response = await fetch(url, { next: { revalidate: 10 } });
     var data = await response.json();
     return data.rates
 }
 
+const loadValues = () => new Promise(resolve => {
+    currencies.forEach(async (currency, index) => {
+        const url = new URL(baseUrl);
+        url.searchParams.append("base", currency.base);
+        url.searchParams.append("symbols", currency.symbols);
+        await getData(url).then((response) => (currency.value = response[currency.symbols]));
+        if (index === currencies.length - 1) resolve();
+    });
+});
+
 async function HeroPreviewTop3() {
-    function start() {
-                currencies.map(async({ name, value, url, code }) => (
-            value = await getData(url),
-            console.log(value.EGP, name),<HeroPreviewTop3/>
-        ))
-    }
-start()
+    await loadValues();
 
     return (
-
         <section className='px-3 py-4'>
-        
-            <div className='flex justify-evenly items-center text-center space-x-3'>
-
+            <div className='flex justify-evenly items-center text-center space-x-3 my-4'>
                 {
-                    currencies.map(({ name, icon, url, value }, index) => (
+                    currencies.map(({ icon, name, value, symbols }, index) => (
                         <div key={index}>
-                            <Image src={icon} width={200} height={200} />
-                            <h1>{name}</h1>
-                            <p>{value.EGP||0}</p>
+                            <Image src={icon} width={200} height={200} priority={true} />
+                            <h1 className='text-4xl text-white'>{name}</h1>
+                            <p className='text-3xl text-white'>{Math.round((value + Number.EPSILON) * 100) / 100} <span className='text-amber-200 text-xl'>{symbols}</span></p>
                         </div>
                     ))
                 }
             </div>
         </section>
-
     )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -6,33 +6,35 @@ import HeroPreviewTop3 from './components/HeroPreviewTop3';
 export default function page() {
   return (
     <>
-          <section className='px-3 py-4 sm:flex sm:flex-row-reverse sm:justify-evenly w-full sm:max-w-7xl sm:m-auto sm:my-12'>
-      <div className='sm:w-1/2'>
-        <Image className='mx-auto mb-4 sm:hidden' src={'/hero-image.png'} width={350} height={350} priority={true} />
-        <Image className='hidden mx-auto mb-4 sm:block' src={'/hero-image.png'} width={500} height={500} priority={true} />
-        <h1 className='text-4xl text-white font-semibold text-center sm:hidden'>Stay up to date with <span className='text-amber-200 font-bold' > Financial State</span></h1>
-        <h2 className='text-lg text-gray-300 font-light tracking-wide text-center my-4 sm:hidden'>Track all cryptocurrencies, gold and Currencies latest prices
+      <section className='px-3 py-4 sm:flex sm:flex-row-reverse sm:justify-evenly w-full sm:max-w-7xl sm:m-auto sm:my-12'>
+        <div className='sm:w-1/2'>
+          <Image className='mx-auto mb-4 sm:hidden' src={'/hero-image.png'} width={350} height={350} priority={true} />
+          <Image className='hidden mx-auto mb-4 sm:block' src={'/hero-image.png'} width={500} height={500} priority={true} />
+          <h1 className='text-4xl text-white font-semibold text-center sm:hidden'>Stay up to date with <span className='text-amber-200 font-bold' > Financial State</span></h1>
+          <h2 className='text-lg text-gray-300 font-light tracking-wide text-center my-4 sm:hidden'>Track all cryptocurrencies, gold and Currencies latest prices
             and news.</h2>
-      </div>
-
-      <div className='w-full sm:w-1/2 flex flex-col justify-center items-center my-2 space-y-2 self-start'>
-        <div className='hidden sm:block '>
-          <h1 className='text-5xl text-white font-medium leading-tight text-start'>Stay up to date with <span className='text-amber-200 font-bold' ><br/> Financial State</span></h1>
-          <h2 className='text-xl text-gray-300 font-light tracking-wide text-center my-4'>Track all cryptocurrencies, gold and Currencies latest prices
-            and news.</h2>
-          <div className='w-full flex space-x-4'>
-
-            <Link href='/preview' className='text-white text-center px-6 py-2 w-1/2 bg-slate-600 rounded-md font-medium tracking-wide text-lg animate-pulse' >Preview</Link>
-            <Link href='/login' className='text-white text-center px-6 py-2 w-1/2 bg-teal-700 rounded-md font-medium tracking-wide text-lg' >Login</Link>
-          </div>
-
         </div>
-        <Link href='/preview' className='sm:hidden text-white text-center px-6 py-2 w-full bg-slate-600 rounded-md font-medium tracking-wide text-lg animate-pulse' >Preview</Link>
-        <Link href='/login' className='sm:hidden text-white text-center px-6 py-2 w-full bg-teal-700 rounded-md font-medium tracking-wide text-lg' >Login</Link>
-      </div>
-    </section>
-    <hr className="sm:mx-6 border-gray-200  dark:border-gray-600 lg:my-8" />
-    <HeroPreviewTop3/>
+
+        <div className='w-full sm:w-1/2 flex flex-col justify-center items-center my-2 space-y-2 self-start '>
+          <div className='hidden sm:block '>
+            <h1 className='text-5xl text-white font-medium leading-tight text-start'>Stay up to date with <span className='text-amber-200 font-bold' ><br /> Financial State</span></h1>
+            <p className='text-xl text-gray-300 font-light tracking-wide my-4 text-start'>Track all cryptocurrencies, gold and Currencies latest prices
+              and news.</p>
+            <p className='text-xl text-gray-300 font-light tracking-wide my-4 text-start'>For Investors who seeks detailed and UpToDate informations.</p>
+            <p className='text-xl text-gray-300 font-light tracking-wide my-4 text-start'>For Amateurs who wants to invest their wealth.</p>
+            <div className='w-full flex space-x-4'>
+
+              <Link href='/preview' className='text-white text-center px-6 py-2 w-1/2 bg-slate-600 rounded-md font-medium tracking-wide text-lg animate-pulse' >Preview</Link>
+              <Link href='/login' className='text-white text-center px-6 py-2 w-1/2 bg-teal-700 rounded-md font-medium tracking-wide text-lg' >Login</Link>
+            </div>
+
+          </div>
+          <Link href='/preview' className='sm:hidden text-white text-center px-6 py-2 w-full bg-slate-600 rounded-md font-medium tracking-wide text-lg animate-pulse' >Preview</Link>
+          <Link href='/login' className='sm:hidden text-white text-center px-6 py-2 w-full bg-teal-700 rounded-md font-medium tracking-wide text-lg' >Login</Link>
+        </div>
+      </section>
+      <hr className="sm:mx-6 border-gray-200  dark:border-gray-600 lg:my-8" />
+      <HeroPreviewTop3 />
     </>
 
   )
